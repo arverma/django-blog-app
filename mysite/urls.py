56004fbced3e9a5e86f4741ceae9927462d4b768
arverma <v.aman@iiitmanipur.ac.in>
2019-03-31 23:34:36 +0530

    Added CSS

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from blog.views import post_list
+from django.urls import path, include
 
 urlpatterns = [
-    path('', post_list),
+    path('', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
