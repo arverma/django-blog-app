2e64a2712d32e0ef8d054fb2181e84c23a5da2ce
Aman Ranjan Verma <v.aman@iiitmanipur.ac.in>
2020-07-25 07:38:51 +0530

    removed secret

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(86rf5)^6^_a@n1xe=gxb-^0our%r&$w#yc-&af1g^6o#hoacn'
+SECRET_KEY = '*************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
